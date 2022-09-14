9425c624fcd3ad25a016f69eed1145c6df525573
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-14 19:06:19 +0630

    feat: add home view

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,84 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./views/NavBar.vue";
+import {Menu, MenuButton, MenuItems, MenuItem} from '@headlessui/vue'
+import {PlusCircleIcon, ChevronDownIcon} from '@heroicons/vue/24/solid'
+
+const statuses = ['Pending', 'Draft', 'Paid']
+
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold text-purple-500">
-    Hello world from tailwindcss
-  </h1>
+  <main class="bg-slate-900 flex flex-row">
+    <NavBar/>
 
+    <div class="mx-auto px-4  text-white mt-8">
+      <div class="fixed top-16 w-56 text-right">
+      </div>
+      <div class="flex flex-row gap-x-96 items-center">
+        <div>
+          <h1 class="text-3xl font-semibold mb-1">Invoices</h1>
+          <div>There are 4 total invoices.</div>
+        </div>
+
+        <div class="flex gap-x-4 items-center">
+          <!--        dropdown menu -->
+          <div>
+            <Menu as="div" class="relative inline-block text-left">
+              <div>
+                <MenuButton
+                    class="inline-flex w-full justify-center rounded-md px-4 py-2  text-white focus:outline-none"
+                >
+                  Filter By Status
+                  <ChevronDownIcon
+                      class="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
+                      aria-hidden="true"
+                  />
+                </MenuButton>
+              </div>
+
+              <transition
+                  enter-active-class="transition duration-100 ease-out"
+                  enter-from-class="transform scale-95 opacity-0"
+                  enter-to-class="transform scale-100 opacity-100"
+                  leave-active-class="transition duration-75 ease-in"
+                  leave-from-class="transform scale-100 opacity-100"
+                  leave-to-class="transform scale-95 opacity-0"
+              >
+                <MenuItems
+                    class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                >
+                  <div class="px-1 py-1">
+                    <MenuItem
+                        v-slot="{ active }"
+                        v-for="(status,index) in statuses"
+                        :key="index"
+                    >
+                      <button
+                          :class="[
+                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                ]"
+                      >
+                        {{ status }}
+                      </button>
+                    </MenuItem>
+                  </div>
+
+                </MenuItems>
+              </transition>
+            </Menu>
+
+          </div>
+          <!--         invoice button -->
+          <div>
+            <button class="btn btn-primary rounded-full flex gap-x-2 items-center">
+              <PlusCircleIcon class="h-8"/>
+              <span class="text-sm">New Invoice</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/views/NavBar.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {DocumentChartBarIcon} from '@heroicons/vue/24/outline'
+</script>
+
+<template>
+  <nav class="bg-slate-800  md:w-20 h-screen flex flex-col rounded-t-2xl">
+    <!--      Invoice Logo -->
+    <div class="text-white bg-violet-600 py-3 flex justify-center items-center rounded-r-2xl">
+      <DocumentChartBarIcon class="h-12 w-12"/>
+    </div>
+  </nav>
+</template>
